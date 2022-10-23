f8bf04b0186b0cee83d8bf867490b1d537bfb85d
Insignificant74 <kunic@A-Plants-PC>
2022-10-23 11:59:54 +0100

    Coal fish

---
+++ src/main/java/com/oreoredstone/orefish/entity/renderer/CoalFishRenderer.java
@@ -0,0 +1,33 @@
+
+package com.oreoredstone.orefish.entity.renderer;
+
+import net.minecraftforge.fml.client.registry.RenderingRegistry;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.client.event.ModelRegistryEvent;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.api.distmarker.Dist;
+
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.entity.Entity;
+import net.minecraft.client.renderer.entity.model.CodModel;
+import net.minecraft.client.renderer.entity.MobRenderer;
+
+import com.oreoredstone.orefish.entity.CoalFishEntity;
+
+@OnlyIn(Dist.CLIENT)
+public class CoalFishRenderer {
+	public static class ModelRegisterHandler {
+		@SubscribeEvent
+		@OnlyIn(Dist.CLIENT)
+		public void registerModels(ModelRegistryEvent event) {
+			RenderingRegistry.registerEntityRenderingHandler(CoalFishEntity.entity,
+					renderManager -> new MobRenderer(renderManager, new CodModel(), 0.5f) {
+
+						@Override
+						public ResourceLocation getEntityTexture(Entity entity) {
+							return new ResourceLocation("ore_fish:textures/entities/coal_fish.png");
+						}
+					});
+		}
+	}
+}
---
+++ src/main/java/com/oreoredstone/orefish/procedures/CoalFishRightClickProcedure.java
@@ -0,0 +1,10 @@
+package com.oreoredstone.orefish.procedures;
+
+import java.util.Map;
+
+public class CoalFishRightClickProcedure {
+
+	public static void executeProcedure(Map<String, Object> dependencies) {
+
+	}
+}
