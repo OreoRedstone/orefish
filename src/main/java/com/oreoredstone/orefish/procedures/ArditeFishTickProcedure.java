70c64a571b44d80fb8304cbff821d0f39c997c5c
Insignificant74 <kunic@A-Plants-PC>
2022-10-23 11:49:42 +0100

    Ardite fish

---
+++ src/main/java/com/oreoredstone/orefish/procedures/ArditeFishTickProcedure.java
@@ -0,0 +1,10 @@
+package com.oreoredstone.orefish.procedures;
+
+import java.util.Map;
+
+public class ArditeFishTickProcedure {
+
+	public static void executeProcedure(Map<String, Object> dependencies) {
+
+	}
+}
